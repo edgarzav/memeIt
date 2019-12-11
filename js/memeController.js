6f0f47dcf97bf1da190c33de2246caa55254dc40
Mona Lisa <edgar.zavadski@gmail.com>
2019-12-11 12:53:56 +0200

    second commit

--- js/memeController.js
+++ js/memeController.js
@@ -1,14 +1,93 @@
-function init(){
+const gCanvas = document.getElementById('my-canvas');
+const gCtx = gCanvas.getContext('2d')
+let gImg;
+
+function init() {
     setImages()
     renderGallery()
+    // drawText('Coding Academy', 20, 80)
 }
-function renderGallery() {
-    let images = getImagesToRender();
 
-    let divs = images.map(function (image) {
-        return `<div class="">
-                <img class="image-item" src="${image.url}" alt="">
+function loadCanvas(id){
+    drawImg(id)// <=
+console.log(id);
+document.querySelector('.gallery-container').style.display = 'none';
+document.querySelector('.meme-edit').style.display = 'block';
+
+}
+
+
+function onSetTxtToMeme(elTxt) {
+    console.log(elTxt.value);
+    addTxtToMeme(elTxt.value);
+    if (gImg) {
+        drawText(gMeme.txts[0].line, 20, 80)
+    }
+
+}
+
+function drawImg(id) {
+    // const img = document.querySelector('img');
+    // gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+    //let ctx = getContext();
+    let imgIndex = getImgIndexById(id);
+
+    if (gImg)
+        gCtx.drawImage(gImg, 0, 0, gCanvas.width, gCanvas.height)
+    else {
+        gImg = new Image()
+        gImg.src = gImgs[imgIndex].url
+        gImg.onload = () => {
+            gCtx.drawImage(gImg, 0, 0, gCanvas.width, gCanvas.height)
+
+        };
+    }
+    // NOTE: the proportion of the image - should be as the canvas,
+    // otherwise the image gets distorted
+}
+
+function drawText(txt, x, y) {
+    console.log(x, ' ', y);
+
+    gCtx.fillStyle = '#fff'
+    gCtx.strokeStyle = 'green'
+    gCtx.lineWidth = 2
+    gCtx.font = "40px Arial";
+    gCtx.fillText(txt, x, y);
+    gCtx.strokeText(txt, x, y);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function renderGallery() {
+    let imgs = getImagesToRender();
+
+    let divs = imgs.map(function (img) {
+        return `<div class="" onclick="loadCanvas(${img.id})">
+                <img class="image-item" src="${img.url}" alt="">
                 </div>`
-    });  
+    });
     document.querySelector('.gallery-container').innerHTML = divs.join()
 }
--- js/memeService.js
+++ js/memeService.js
@@ -1,4 +1,62 @@
-let gImages = [];
+var gKeywords = { 'happy': 12, 'funny puk': 1 }
+var gImgs = [{ id: 2, url: '../meme-imgs/005.jpg', keywords: ['happy'] }];
+var gMeme = {
+    selectedImgId: 1,
+    selectedTxtIdx: 0,
+    txts: [{
+        line: 'I never eat Falafel', size: 20,
+        align: 'left',
+        color: 'red'
+    }]
+}
+function getContext() {
+    return gCtx;
+}
+
+
+function addTxtToMeme(txt) {
+    gMeme.selectedImgId = 0
+    gMeme.txts[0].line = txt;
+}
+
+function getImgIndexById(id){
+    return gImgs.findIndex(function(img){
+    return img.id === id
+    })
+}
+
+
+
+
+// function loadCanvas() {
+//     gCanvas = document.getElementById('my-canvas');
+//     ctx = gCanvas.getContext('2d')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let idx = 0;
 
 function setImage(url) {
@@ -9,15 +67,15 @@ function setImage(url) {
 }
 
 
-function setImages(){
-   gImages.push(setImage('../meme-imgs/003.jpg'));
-   gImages.push(setImage('/meme-imgs/004.jpg'));
+function setImages() {
+    gImgs.push(setImage('../meme-imgs/003.jpg'));
+    gImgs.push(setImage('/meme-imgs/004.jpg'));
 
 }
 
-function getImagesToRender(){
-    console.log(gImages.length);
-    console.log(gImages);
-    
-    return gImages
+function getImagesToRender() {
+    console.log(gImgs.length);
+    console.log(gImgs);
+
+    return gImgs
 }
